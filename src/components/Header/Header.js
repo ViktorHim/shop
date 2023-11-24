ea83f3cb44ab236d5135116eb8b88dc1b05d1e0b
Viktor Himlih <vhimlihmail@gmail.com>
2023-11-25 01:16:04 +0300

    modal window

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,19 @@ import './Header.scss'
 import cart from '../../icons/header/cart.svg'
 import profile from '../../icons/header/profile.svg'
 import logo from '../../icons/header/logo.svg'
+import Modal from '../Modal/Modal'
 
 import SearchBar from '../SearchBar/SearchBar';
 import {Link } from 'react-router-dom';
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const switchModal = () => {
+        setShowModal(showModal => !showModal);
+    }
+
     return(
     <header className="header">
         <div className="container">
@@ -15,11 +23,13 @@ const Header = () => {
                 <SearchBar placeholder="Что будем искать?"/>
                 <ul className="actions">
                     <li><Link to="/shopping_cart" className="action__item"><img src={cart} alt ="cart"></img></Link></li>
-                    <li><a href="#" className="action__item"><img src={profile} alt ="profile"></img></a></li>
+                    <li><button className="action-login"
+                    onClick={switchModal}><img src={profile} alt ="profile"></img></button></li>
                     <li><a href="#" className="action__lang">ru</a></li>
                 </ul>
             </nav>
         </div>
+        <Modal showModal={showModal} setShowModal={setShowModal}/>
     </header>
     );
 }
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,60 @@
+
+import './Modal.scss'
+import close from '../../assets/icons/modal/close.svg'
+import { useCallback, useEffect, useRef } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const Modal = ({showModal, setShowModal}) => {
+    const modalRef = useRef();
+
+    const animation = useSpring({
+        config: {
+            duration: 250
+        },
+        opacity: showModal ? 1 : 0,
+        transform: showModal ? `translateX(0%)` : `translateX(100%)`
+    });
+
+    const closeModal = e => {
+        if (modalRef.current === e.target) {
+          setShowModal(false);
+        }
+      };
+
+    const keyPress = useCallback(
+        e => {
+            if(e.key === 'Escape' && showModal) {
+                setShowModal(false);
+            }
+        },
+        [setShowModal, showModal]
+    );
+
+    useEffect(
+        () => {
+            document.addEventListener('keydown', keyPress);
+            return () => document.removeEventListener('keydown', keyPress);
+        },
+        [keyPress]
+    );
+
+  return (
+    <>
+    {showModal ? (
+        <div className="container">
+            <div className="modal" onClick={closeModal} ref={modalRef}>
+                <animated.div style={animation}>
+                <div className="modal__container">
+                    <button className="modal__close"
+                    onClick={() => setShowModal(showModal => !showModal)}><img src={close} alt="close" /></button>
+                    <div className="modal__content"></div>
+                </div>
+                </animated.div>
+            </div>
+        </div>
+    ) : null}
+    </>
+  )
+}
+
+export default Modal
